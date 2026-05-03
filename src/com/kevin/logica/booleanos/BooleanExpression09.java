992e69bd590523cc03fa27e05c14c5c8f2871eab
Kevin Valone Brilhante <kevinvbrilhante@gmail.com>
2026-05-02 23:05:38 -0300

    feat: add boolean expression 09

---
+++ src/com/kevin/logica/booleanos/BooleanExpression09.java
@@ -0,0 +1,19 @@
+package com.kevin.logica.booleanos;
+
+// Problem: Evaluate a boolean expression
+// Input: none (predefined values)
+// Output: boolean result (true or false)
+
+public class BooleanExpression09 {
+    public static void main(String[] args) {
+
+        // Processing
+        boolean result = (2 == 2 || !(3 < 1) && 5 >= 5 || 8 > 12);
+
+        // Output
+        System.out.println("Result: " + result);
+
+        // Expected: True
+    }
+
+}
